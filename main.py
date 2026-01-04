470f73633f69c94418369523e0322f1f99d934bf
luho91 <lukas.holzheid@mail.de>
2026-01-04 14:51:38 +0100

    Fix syntax

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def main():
 
             for shot in shots:
                 if shot.collides_with(asteroid):
-                log_event("asteroid_shot")
+                    log_event("asteroid_shot")
                     asteroid.kill()
                     shot.kill()
 
